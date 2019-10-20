af7ea461fb314c912706be87a4812996675cc6ce
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-20 21:31:57 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -20,3 +20,17 @@ attr_accessor :email_addresses
       end
     end
 end
+
+#FLATIRON SOLUTION
+
+# class EmailAddressParser
+#   attr_reader :emails
+#
+#   def initialize(emails)
+#     @emails = emails
+#   end
+#
+#   def parse
+#     emails.split(/, | /).uniq
+#   end
+# end
